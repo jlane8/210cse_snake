e4a6ebd60f27eb92b6321c4ba52f1693c3143b0b
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-06 10:15:44 -0500

    added execute method

--- snake/game/scripting/move_actors_action.py
+++ snake/game/scripting/move_actors_action.py
@@ -1,8 +1,17 @@
 from game.scripting.action import Action
 
-
+class MoveActorsAction(Action):
+    
 # TODO: Implement MoveActorsAction class here! 
+    def execute(self, cast, script):
+        """
 
+        """
+        self._actors = cast.get_all_actors()
+        self._actions = script.get_actions("snakes")
+        for actor in self._actors:
+            actor.move_next()
+        return 
 # Override the execute(cast, script) method as follows:
 # 1) get all the actors from the cast
 # 2) loop through the actors
