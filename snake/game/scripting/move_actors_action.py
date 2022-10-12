ded6a157d667117e79ba8cc3805235761c0ce031
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-12 11:39:14 -0500

    cleaned up unused code

--- snake/game/scripting/move_actors_action.py
+++ snake/game/scripting/move_actors_action.py
@@ -1,18 +1,22 @@
 from game.scripting.action import Action
 
-class MoveActorsAction(Action):
-    
 # TODO: Implement MoveActorsAction class here! 
+class MoveActorsAction(Action):
+    """
+    The MoveActorsAction class is a child class of Action. It exists simply
+    to overwrite or override the Action execute() method.
+    """
+
     def execute(self, cast, script):
         """
-
+        Parameters: cast - contains all of those in the cast, including actors
+                    script - unused
+        Return: nothing
+        This method overrides the Actor execute() method to move the actors
+        around the game window by calling the actor.move_next() method on
+        all actors. 
         """
         self._actors = cast.get_all_actors()
-        self._actions = script.get_actions("snakes")
         for actor in self._actors:
             actor.move_next()
         return 
-# Override the execute(cast, script) method as follows:
-# 1) get all the actors from the cast
-# 2) loop through the actors
-# 3) call the move_next() method on each actor
